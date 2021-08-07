bfd7effba2028979b5eaeef3b86ca7beb180742f
nishant Gupta <ngactive99@gmail.com>
2021-08-07 12:19:09 +0530

    added an image and learnt more about jsx and how to write jsx using html in js

--- src/FirstComponent.js
+++ src/FirstComponent.js
@@ -1,12 +1,25 @@
 import React from 'react' // to build a component we will import react using es6 format 
 import { render} from "react-dom"; // we will use this to render the method to index.js root 
 
+
 function FirstComponent() {
     return ( // you always need to return the function if we dont then there will be an error which states that nothing has been returned by the component 
+        <React.Fragment> 
         <div>
             <h1>This is out first React component using function component</h1>
+            <img src="istock-147290529.jpg" alt="Penguines should have been here but they got stuck somewhere!" width="500" height="333"></img>
+            <ul>
+                <li>
+                    <h3>I like penguines</h3>
+                </li>
+                <li>
+                    <h3>I dont like penguines</h3>
+                </li>
+            </ul>
             
         </div>
+         //<div></div> // show this to show that we cannot a div outside  div inside the return everything inside the return should be wrapped so we cn use react fragment 
+        </React.Fragment> //  shortcut to do this is using just <> </> 
     )
 }
 
