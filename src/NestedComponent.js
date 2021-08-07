eab3c332238b3f5b418846c9f1280693497b5630
nishant Gupta <ngactive99@gmail.com>
2021-08-07 16:24:57 +0530

    introduction to nested components how to use them.

---
+++ src/NestedComponent.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+//we will learn nested components and react tools
+
+function NestedComponent() {
+    return (
+        <div>            
+             <h1>This is the component 1</h1>
+             <Component2 /> 
+        </div>
+    )
+}
+
+const Component2   = () => { // and start the name of your component or function with a capital letter & here we have created a nested component but if we run this without calling this component inside the return of our first component this wont run / return
+    return (
+        <div>
+            <h2> this is my second-nested component and i have used an arrow function </h2>
+        </div>
+    )
+}
+
+export default NestedComponent
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import FirstComponent from "./FirstComponent";
 import Userform from "./Userform";
+import NestedComponent from "./NestedComponent";
 
 ReactDOM.render(
   <React.StrictMode>
     <Userform />
     <FirstComponent />
+    <NestedComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
