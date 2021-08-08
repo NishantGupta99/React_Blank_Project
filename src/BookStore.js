4145dea13c7592a45d936b6baeab89b4eb45c66b
nishant Gupta <ngactive99@gmail.com>
2021-08-08 16:46:25 +0530

    added css using jsx and learnt how we use js inside jsx

--- src/BookStore.js
+++ src/BookStore.js
@@ -30,7 +30,7 @@ function BookStore() { // the parent component
      <h1>Rich Dad Poor Dad</h1>
  ;
  const Author = () => 
-     <h4>Robert T. Kiyosaki</h4>
+     <h4 style={{ color: '#617d98', fontSize:'0.75 rem', marginTop:'0.25rem'}}>Robert T. Kiyosaki</h4> // this is a way of adding css directly to the the jsx file instead of creating another css file and this has more authorithy hence this can over ride whats written in plain css , we have used a second set of curly braces because we are using javascript syntax inside jsx
  ;
 
 
