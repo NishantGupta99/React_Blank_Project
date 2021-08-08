5f13058afa54d9027156e8febfafddc8237f5e08
nishant Gupta <ngactive99@gmail.com>
2021-08-08 14:17:40 +0530

    added basic css to the page and saw the difference and learnt how to import css into the main file.

--- src/BookStore.js
+++ src/BookStore.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function BookStore() { // the parent component 
     return (
         <section> 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 //import Userform from "./Userform";
 //import NestedComponent from "./NestedComponent";
 import BookStore from "./BookStore";
+import './BookStore.css'; // this is how we import a css file we use the relative path and use the endtire path instead of just the name the naming convention of css is that we name it teh saem what its styling 
 
 ReactDOM.render(
   <React.StrictMode>
