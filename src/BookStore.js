a5a78d35d9bb24d7473dbdacd0c4595fab4c6c49
nishant Gupta <ngactive99@gmail.com>
2021-08-08 17:04:00 +0530

    learned the correct way to comment inside a jsx return statement

--- src/BookStore.js
+++ src/BookStore.js
@@ -5,11 +5,7 @@ function BookStore() { // the parent component
     return (
         <section className= "List"> 
             <BookDetails />
-            <BookDetails />
-            <BookDetails />
-            <BookDetails />
-            <BookDetails />
-            <BookDetails />
+           
         </section>    
         
     ) // we can call a component as many times we want and reuse the component 
@@ -17,20 +13,16 @@ function BookStore() { // the parent component
  const BookDetails = () => {
      return (
          <article className='book'>
-             <Image></Image>
-             <Title></Title>
-             <Author></Author>
+              <img src= 'https://images-na.ssl-images-amazon.com/images/I/51wOOMQ+F3L._SX312_BO1,204,203,200_.jpg' alt =''></img>
+              <h1>Rich Dad Poor Dad</h1>
+              <h4 style={{ color: '#617d98', fontSize:'0.75 rem', marginTop:'0.25rem'}}>Robert T. Kiyosaki</h4>    
+              {/* // this is a way of adding css directly to the the jsx file instead of creating another css file and this has more authorithy hence this can over ride whats written in plain css , we have used a second set of curly braces because we are using javascript syntax inside jsx */}
          </article>
+      
      );
  } 
- const Image = () => (
-     <img src= 'https://images-na.ssl-images-amazon.com/images/I/51wOOMQ+F3L._SX312_BO1,204,203,200_.jpg' alt =''></img>
- );
- const Title = () => 
-     <h1>Rich Dad Poor Dad</h1>
- ;
- const Author = () => 
-     <h4 style={{ color: '#617d98', fontSize:'0.75 rem', marginTop:'0.25rem'}}>Robert T. Kiyosaki</h4> // this is a way of adding css directly to the the jsx file instead of creating another css file and this has more authorithy hence this can over ride whats written in plain css , we have used a second set of curly braces because we are using javascript syntax inside jsx
+
+     
  ;
 
 
