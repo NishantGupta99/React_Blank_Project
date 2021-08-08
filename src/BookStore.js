1cc6e5ac4e8e680c9fb420c1d874429fceefe759
nishant Gupta <ngactive99@gmail.com>
2021-08-08 18:10:00 +0530

    learnet how to use props and the used objects to store the infor mation of components and passed them through the components reusing the component

--- src/BookStore.js
+++ src/BookStore.js
@@ -1,32 +1,44 @@
 import React from 'react'
+const FirstBook = {
+    BookName : "Rich Dad Poor Dad",
+    author : "Robert T. Kiyosaki",
+    img : 'https://images-na.ssl-images-amazon.com/images/I/51wOOMQ+F3L._SX312_BO1,204,203,200_.jpg'
+}
+const SecondBook = {
+    BookName : "The Psychology of Money",
+    author : "Morgan Housel",
+    img : 'https://images-na.ssl-images-amazon.com/images/I/41cWqh0OeQL._SX321_BO1,204,203,200_.jpg'
+} // we have created objects that consists of all the information of each book and then we pass them through the component using props well we can use any other key word instead of props but it is the convention 
 
 
 function BookStore() { // the parent component 
     return (
-        <section className= "List"> 
-            <BookDetails />
-           
-        </section>    
-        
+        <section className="List">
+            <BookDetails img={FirstBook.img}
+                BookName={FirstBook.BookName} author={FirstBook.author} />
+            <BookDetails img={SecondBook.img}
+                BookName={SecondBook.BookName} author={SecondBook.author} />    
+                {/* we are passing the object value into the calling of component  */}
+
+        </section>
+
     ) // we can call a component as many times we want and reuse the component 
 }
-const BookName = "Rich Dad Poor Dad";
-const author = "Robert T. Kiyosaki"
-const img = 'https://images-na.ssl-images-amazon.com/images/I/51wOOMQ+F3L._SX312_BO1,204,203,200_.jpg';
- const BookDetails = () => {
-     return (
-         <article className='book'>
-              <img src= {img} alt =''></img>
-              <h1>{BookName}</h1>
-              <h4 style={{ color: '#617d98', fontSize:'0.75 rem', marginTop:'0.25rem'}}> {author}</h4>    
-              {/* // this is a way of adding css directly to the the jsx file instead of creating another css file and this has more authorithy hence this can over ride whats written in plain css , we have used a second set of curly braces because we are using javascript syntax inside jsx */}
-         </article>
-      
-     );
- } 
 
-     
- ;
+const BookDetails = (props) => {
+    return (
+        <article className='book'>
+            <img src={props.img} alt=''></img>
+            <h1>{props.BookName}</h1>
+            <h4 style={{ color: '#617d98', fontSize: '0.75 rem', marginTop: '0.25rem' }}> {props.author}</h4>
+            {/* // this is a way of adding css directly to the the jsx file instead of creating another css file and this has more authorithy hence this can over ride whats written in plain css , we have used a second set of curly braces because we are using javascript syntax inside jsx */}
+        </article>
+
+    );
+}
+
+
+    ;
 
 
 export default BookStore
